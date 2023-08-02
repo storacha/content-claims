fd8a046665f9ce602afadc157233eb50dfb91547
Alan Shaw <alan.shaw@protocol.ai>
2023-08-02 18:51:13 +0200

    fix: key content on multihash not CID

--- stacks/bucket.js
+++ stacks/bucket.js
@@ -4,6 +4,6 @@ import { Bucket as S3Bucket } from 'sst/constructs'
  * @param {import('sst/constructs').StackContext} config
  */
 export function Bucket ({ stack }) {
-  const claimsBucket = new S3Bucket(stack, 'claims')
+  const claimsBucket = new S3Bucket(stack, 'claims-v1')
   return { claimsBucket }
 }
