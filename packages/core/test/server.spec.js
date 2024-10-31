07abcb2e2d4cf138b419e8c12e99a197105f30fa
Alan Shaw <alan@storacha.network>
2024-10-31 14:01:40 +0000

    fix: content claims derives

--- packages/core/test/server.spec.js
+++ packages/core/test/server.spec.js
@@ -6,6 +6,7 @@ import { connect } from '@ucanto/client'
 import { mock } from 'node:test'
 import * as Block from 'multiformats/block'
 import { sha256, sha512 } from 'multiformats/hashes/sha2'
+import { base58btc } from 'multiformats/bases/base58'
 import * as Bytes from 'multiformats/bytes'
 import * as Link from 'multiformats/link'
 import * as dagCBOR from '@ipld/dag-cbor'
@@ -124,10 +125,17 @@ export const test = {
 
   'should claim location': async (/** @type {import('entail').assert} */ assert) => {
     const { claimStore, signer, server } = await beforeEach()
+    const alice = await ed25519.generate()
 
     const content = await Block.encode({ value: 'find me', hasher: sha256, codec: dagCBOR })
     const car = CAR.codec.encode({ roots: [content] })
     const carBlock = await Block.encode({ value: car, hasher: sha256, codec: CAR.codec })
+
+    const proof = await Assert.location.delegate({
+      issuer: signer,
+      audience: alice,
+      with: signer.did()
+    })
 
     const connection = connect({
       id: signer,
@@ -137,13 +145,14 @@ export const test = {
 
     const result = await Assert.location
       .invoke({
-        issuer: signer,
+        issuer: alice,
         audience: signer,
         with: signer.did(),
         nb: {
           content: { digest: carBlock.cid.multihash.bytes },
           location: ['http://localhost:3000/']
-        }
+        },
+        proofs: [proof]
       })
       .execute(connection)
 
@@ -164,5 +173,125 @@ export const test = {
 
     assert.ok(cap)
     assert.equal(cap.nb.location.toString(), 'http://localhost:3000/')
+  },
+
+  'should not authorize resource (with) constraint violation': async (/** @type {import('entail').assert} */ assert) => {
+    const { signer, server } = await beforeEach()
+    const alice = await ed25519.generate()
+    const bob = await ed25519.generate()
+    const content = await Block.encode({ value: 'find me', hasher: sha256, codec: dagCBOR })
+
+    const proof = await Assert.location.delegate({
+      issuer: signer,
+      audience: alice,
+      with: signer.did()
+    })
+
+    const connection = connect({
+      id: signer,
+      codec: CAR.outbound,
+      channel: server
+    })
+
+    const result = await Assert.location
+      .invoke({
+        issuer: alice,
+        audience: signer,
+        with: bob.did(),
+        nb: {
+          content: content.cid,
+          location: ['http://localhost:3000/']
+        },
+        proofs: [proof]
+      })
+      .execute(connection)
+
+    assert.ok(result.out.error)
+    assert.ok(result.out.error.message.includes(`Can not derive ${Assert.location.can} with ${bob.did()} from ${signer.did()}`))
+  },
+
+  'should not authorize location caveats content constraint violation': async (/** @type {import('entail').assert} */ assert) => {
+    const { signer, server } = await beforeEach()
+    const alice = await ed25519.generate()
+
+    const content = await Block.encode({ value: 'find me', hasher: sha256, codec: dagCBOR })
+    const car = CAR.codec.encode({ roots: [content] })
+    const carBlock = await Block.encode({ value: car, hasher: sha256, codec: CAR.codec })
+
+    const proof = await Assert.location.delegate({
+      issuer: signer,
+      audience: alice,
+      with: signer.did(),
+      nb: {
+        content: { digest: carBlock.cid.multihash.bytes }
+      }
+    })
+
+    const connection = connect({
+      id: signer,
+      codec: CAR.outbound,
+      channel: server
+    })
+
+    const result = await Assert.location
+      .invoke({
+        issuer: alice,
+        audience: signer,
+        with: signer.did(),
+        nb: {
+          content: content.cid,
+          location: ['http://localhost:3000/']
+        },
+        proofs: [proof]
+      })
+      .execute(connection)
+
+    assert.ok(result.out.error)
+    assert.ok(result.out.error.message.includes(`Constraint violation: content: ${base58btc.encode(content.cid.multihash.bytes)} violates ${base58btc.encode(carBlock.cid.multihash.bytes)}`))
+  },
+
+  'should not authorize index caveats index constraint violation': async (/** @type {import('entail').assert} */ assert) => {
+    const { signer, server } = await beforeEach()
+    const alice = await ed25519.generate()
+
+    const content = await Block.encode({ value: 'find me', hasher: sha256, codec: dagCBOR })
+    const car = CAR.codec.encode({ roots: [content] })
+    const carBlock = await Block.encode({ value: car, hasher: sha256, codec: CAR.codec })
+
+    const index = await BlobIndexUtil.fromShardArchives(content.cid, [car])
+    const indexBytes = Result.unwrap(await index.archive())
+    const indexLink = Link.create(CAR.codec.code, await sha256.digest(indexBytes))
+
+    const proof = await Assert.index.delegate({
+      issuer: signer,
+      audience: alice,
+      with: signer.did(),
+      nb: {
+        content: content.cid,
+        index: indexLink
+      }
+    })
+
+    const connection = connect({
+      id: signer,
+      codec: CAR.outbound,
+      channel: server
+    })
+
+    const result = await Assert.index
+      .invoke({
+        issuer: alice,
+        audience: signer,
+        with: signer.did(),
+        nb: {
+          content: content.cid,
+          index: content.cid
+        },
+        proofs: [proof]
+      })
+      .execute(connection)
+
+    assert.ok(result.out.error)
+    assert.ok(result.out.error.message.includes(`Constraint violation: index: ${content.cid} violates ${indexLink}`))
   }
 }
